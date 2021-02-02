ad95e65b2a91d5a59c363b606b8f647f81a7de54
Vangual <282254+vangual@users.noreply.github.com>
2021-02-02 15:49:11 +0100

    Add initial testcase

---
+++ apiserver/test.py
@@ -0,0 +1,19 @@
+import unittest
+import flask_unittest
+from app import app
+
+
+class DemoTests(flask_unittest.ClientTestCase):
+    url_base = '/v1.0'
+    app = app.app  # The flask webapp is a sub-object to the app object. We save a reference to it for quicker access
+
+    def test_1_hello(self, client):
+        tester = client
+        response = tester.get(self.url_base + '/hello')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('text/plain', response.content_type)
+        self.assertInResponse(b'Hello World', response)
+
+
+if __name__ == '__main__':
+    unittest.main()
