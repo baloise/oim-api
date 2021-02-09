9164ce9bd951375e5957628d960cb3661a817aed
B041091 <b041091@balgroupit.com@svx-jmpp001.linux.balgroupit.com>
2021-02-09 17:12:15 +0100

    fix findings

--- apiserver/api/OrderHandlerTester.py
+++ apiserver/api/OrderHandlerTester.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 
 class OrderHandler:
 
+
   def parseFile(self, filename):
     tree = ET.parse(filename)
     root = tree.getroot()
@@ -10,13 +11,15 @@ class OrderHandler:
       for subelem in elem:
         print(subelem.attrib)
 
+
   def listItems(self, tag):
     tree = ET.parse(self.filename)
     root = tree.getroot()
     for elem in root:
       for subelem in elem.findall(tag):
         print(subelem.get('name'))
-  
+
+
   def listOrders(self, orderno=None):
     tree = ET.parse(self.filename)
     root = tree.getroot()
@@ -24,8 +27,10 @@ class OrderHandler:
       status=elem.find('status').text
       print("Orderno: {0}, Status: {1}".format(elem.get('nr'), status))
 
+
   def __init__(self, filename):
     self.filename=filename
+
 
 writer=OrderHandler('order_data.xml')
 #writer.parseFile('order_data.xml')
