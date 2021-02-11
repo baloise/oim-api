c4b2c8d7f4bdd7d7cd1df8e8daa3413c65a516e5
Vangual <vangual@users.noreply.github.com>
2021-02-11 14:49:59 +0100

    PEP8 indentation fix

--- apiserver/api/demo.py
+++ apiserver/api/demo.py
@@ -90,11 +90,10 @@ def basic_auth(username, password, required_scopes=None):
 
     # optional
     if required_scopes is not None and not validate_scope(required_scopes, info['scope']):
-        raise OAuthScopeProblem(
-                description='Provided user doesn\'t have the required access rights',
-                required_scopes=required_scopes,
-                token_scopes=info['scope']
-            )
+        raise OAuthScopeProblem(description='Provided user doesn\'t have the required access rights',
+                                required_scopes=required_scopes,
+                                token_scopes=info['scope']
+                                )
 
     return info
 
