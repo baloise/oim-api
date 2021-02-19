cdfb79639e1cc048b3cd4a3c0c4a2639f16c6d73
Vangual <vangual@users.noreply.github.com>
2021-02-19 18:06:01 +0100

    Add intial db compontent

---
+++ apiserver/components/db.py
@@ -0,0 +1,26 @@
+from app import application as app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
+import logging
+import os
+
+
+def setup_db(db_to_setup):
+    if not db_to_setup or not db_to_setup.engine:
+        logging.error('setup_db called but no valid db object given. Connection issue?')
+
+    if not db.engine.table_names():
+        logging.info('Detected empty database, proceeding to create structure..')
+        try:
+            db.create_all()
+        except SQLAlchemyError as e:
+            logging.exception('Error creating database structure. Msg: {msg}'.format(msg=str(e)))
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI') or 'sqlite://:memory:'
+db = SQLAlchemy(app)
+
+db.ap
+
+# Setup basic structure if it isn't there already in the db.
+setup_db(db)
