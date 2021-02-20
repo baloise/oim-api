684bf1b2b3b74d844a6691d3d3d79f065f36130d
MegaRusher <manuel.masse@gmail.com>
2021-02-20 08:50:44 +0100

    Orchestra interfacing  get_item w custom filter

--- apiserver/orchestra/OrchestraRequestHandler.py
+++ apiserver/orchestra/OrchestraRequestHandler.py
@@ -3,14 +3,11 @@ from zeep import Client
 # from zeep.transports import Transport
 # from requests import Session
 # from requests.auth import HTTPBasicAuth
-
-orchestra_cmdb_webservice = 'http://x10066984.balgroupit.com:8819/get_cmdb_item?wsdl'
-
 # session = Session()
 # session.auth = HTTPBasicAuth('b000', 'pass')
 
 
-class OrchestraRequestHandler():
+class OrchestraRequestHandler():        # This class knows SOAP
     def __init__(self, url):
         self.soap_client = Client(url)
 
@@ -18,28 +15,43 @@ class OrchestraRequestHandler():
         print('Namespace: ', self.soap_client.namespaces)
         print('Operations: ', self.soap_client.service._operations)
 
-    def get_item(self, id):
-        return self.soap_client.service.get_item(id)
+    def get_item_by_id(self, id):
+        return self.soap_client.service.get_item_by_id(id)
+
+    def get_item(self, filter):
+        return self.soap_client.service.get_item(**filter)
 
 
 class GenericCmdbHandler(ABC):
     @abstractmethod
-    def get_item(self, id): pass
+    def get_item_by_id(self, id): pass
+
+    def delete_item(self, id): pass     # Only update active_date
+
+    def add_item(self): pass
+
+    @abstractmethod
+    def get_item(self, filter): pass
 
 
 class OrchestraCmdbHandler(GenericCmdbHandler):     # This class has no idea of SOAP
     def __init__(self):
-        self.orchestra = OrchestraRequestHandler(orchestra_cmdb_webservice)
+        self.url = 'http://x10066984.balgroupit.com:8819/cmdb_request?wsdl'
+        self.orchestra = OrchestraRequestHandler(self.url)
 
     def get_item_by_id(self, id):
-        return self.orchestra.get_item(id)
+        return self.orchestra.get_item_by_id(id)
 
     def get_item(self, field, pattern):
-        return self.orchestra.get_item(id)
+        xml_filter = {'field':field, 'pattern':pattern}
+        print(xml_filter['field'])
+        return self.orchestra.get_item(xml_filter)
 
 
 if __name__ == '__main__':
     cmdb_h = OrchestraCmdbHandler()
     cmdb_h.orchestra.list_operations()
     for item in cmdb_h.get_item_by_id('instance@svw-blablat001.balgroupit.com'):
+        print(item)
+    for item in cmdb_h.get_item('type','system'):
         print(item)
