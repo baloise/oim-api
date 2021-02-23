62bb6ebb690f5cedfcf1bfd882f8e79153c9ae34
matal <74904755+matal0@users.noreply.github.com>
2021-02-23 12:51:05 +0100

    add methods to get requests details (#19)
    * list request parameters, get request parameters
    
    * line too long

--- apiserver/ocRun.py
+++ apiserver/ocRun.py
@@ -5,5 +5,13 @@ load_dotenv()
 
 handler = OurCloudRequestHandler.getInstance()
 reqno = 93
-ocstatus = handler.GetCustomTableRecordsByName(reqno)
+ocstatus = handler.get_request_status(reqno)
 print("Request {reqno} has status: {ocstatus}".format(ocstatus=ocstatus, reqno=reqno))
+
+
+ocstatus = handler.list_extended_request_parameters(reqno)
+print("Request {reqno} has extended parameters: {ocstatus}".format(ocstatus=ocstatus, reqno=reqno))
+
+extendedParams = ["RequestDetailID", "InstanceSize", "hdnOSType"]
+ocstatus = handler.get_extended_request_parameters(reqno, extendedParams)
+print("Request {reqno} extended parameters values: {ocstatus}".format(ocstatus=ocstatus, reqno=reqno))
--- apiserver/ourCloud/OurCloudHandler.py
+++ apiserver/ourCloud/OurCloudHandler.py
@@ -1,4 +1,5 @@
 import requests
+import urllib3
 import os
 from ourCloud.auth import TokenAuthHandler
 import json
@@ -21,16 +22,103 @@ class OurCloudRequestHandler:
         else:
             OurCloudRequestHandler.__instance = self
             self.auth = TokenAuthHandler(self)
+            urllib3.disable_warnings()
 
-    def get_all_request_details(self, requestno):
+    def get_extended_request_parameters(self, requestno, parameters: list):
         # 3.4.5
-        pass
+        if not requestno:
+            raise ValueError("No request number provided")
+
+        method_url = '{baseUrl}/Requests/RequestDetails/OrgEntityID{orgEntityId}/PlatformEntityID{platformEntityId}/RequestNo/{requestNo}/RequestDetailId{requestDetailId}'.format(  # noqa: E501
+            baseUrl=self.get_base_url(),
+            orgEntityId=self.getOrgEntityId(),
+            platformEntityId=self.getPlatformEntityId(),
+            requestNo=requestno,
+            requestDetailId="/-1"
+        )
+
+        payload = {}
+        headers = {
+            'Authorization': 'Bearer ' + self.getCurrentToken()
+        }
+
+        responseRaw = requests.request("GET", method_url, headers=headers, data=payload, verify=False)
+        jsonString = responseRaw.json()['Result']
+        jsonObj = json.loads(jsonString)
+
+        # 1. outer filter all request details
+        method_json_query = "Table[].AllRequestDetails[]|[0]"
+        outer = self.getResultJson(responseRaw, method_json_query)
+
+        # 2. inner filter requestno and attributes
+        # method_json_query_inner = "[?RequestNo == `{no}`].{attribute}|[0]".format(no=requestno,
+        #  attribute='[LocationName, RequestedStatusText, RequestDetailID]')
+        jsonObj = json.loads(outer)
+        # requestStatus = jmespath.search(method_json_query_inner, jsonObj)
+
+        # 3. inner filter requestno and extended parameters
+        method_json_query_extended = "\
+            [?RequestNo == `{no}`].{attribute}|[0]".format(no=requestno,
+                                                           attribute='[RequestDetailsExtendedParameter]')
+        extendedParameters = jmespath.search(method_json_query_extended, jsonObj)
+
+        # 5. filter extended parameters by given keys
+        extendedKeys = {}
+        for key in parameters:
+            method_json_query_extended_keys = "[?KeyName=='{key}'].KeyValue|[0]".format(key=key)
+            extendedValue = jmespath.search(method_json_query_extended_keys, extendedParameters[0])
+            extendedKeys[key] = extendedValue
+
+        return extendedKeys
+
+    def list_extended_request_parameters(self, requestno):
+        # 3.4.5
+        if not requestno:
+            raise ValueError("No request number provided")
+
+        method_url = '{baseUrl}/Requests/RequestDetails/OrgEntityID{orgEntityId}/PlatformEntityID{platformEntityId}/RequestNo/{requestNo}/RequestDetailId{requestDetailId}'.format(  # noqa: E501
+            baseUrl=self.get_base_url(),
+            orgEntityId=self.getOrgEntityId(),
+            platformEntityId=self.getPlatformEntityId(),
+            requestNo=requestno,
+            requestDetailId="/-1"
+        )
+
+        payload = {}
+        headers = {
+            'Authorization': 'Bearer ' + self.getCurrentToken()
+        }
+
+        responseRaw = requests.request("GET", method_url, headers=headers, data=payload, verify=False)
+        jsonString = responseRaw.json()['Result']
+        jsonObj = json.loads(jsonString)
+
+        # 1. outer filter all request details
+        method_json_query = "Table[].AllRequestDetails[]|[0]"
+        outer = self.getResultJson(responseRaw, method_json_query)
+
+        # 2. inner filter requestno and attributes
+        # method_json_query_inner = "[?RequestNo == `{no}`].{attribute}|[0]".format(no=requestno,
+        #  attribute='[LocationName, RequestedStatusText, RequestDetailID]')
+        jsonObj = json.loads(outer)
+        # requestStatus = jmespath.search(method_json_query_inner, jsonObj)
+
+        # 3. inner filter requestno and extended parameters
+        method_json_query_extended = "\
+            [?RequestNo == `{no}`].{attribute}|[0]".format(no=requestno,
+                                                           attribute='[RequestDetailsExtendedParameter]')
+        extendedParameters = jmespath.search(method_json_query_extended, jsonObj)
+
+        # 4. filter key names of extended parameters
+        method_json_query_extended_keys = "[*].{attribute}".format(attribute='KeyName')
+        extendedKeys = jmespath.search(method_json_query_extended_keys, extendedParameters[0])
+
+        return extendedKeys
 
     def get_request_status(self, requestno):
         # 3.4.4
         if not requestno:
             raise ValueError("No request number provided")
-        self.requestno = requestno
 
         method_url = '{baseUrl}/Requests/Request'.format(
             baseUrl=self.get_base_url()
@@ -42,7 +130,9 @@ class OurCloudRequestHandler:
         }
 
         responseRaw = requests.request("GET", method_url, headers=headers, data=payload, verify=False)
-        return self.parseJson(responseRaw, 'Status')
+
+        method_json_query = "[?RequestNo == `{no}`].{attribute}|[0]".format(no=requestno, attribute='Status')
+        return self.getResultJson(responseRaw, method_json_query)
 
     def getProxy(self):
         proxies = {
@@ -75,7 +165,10 @@ class OurCloudRequestHandler:
     def getCurrentToken(self):
         return self.auth.getToken()
 
-    def parseJson(self, responseRaw, attribute="*"):
+    def getPlatformEntityId(self):
+        return "/VMWAR-15CFFB35-7FC6-449C-9F7F-1CF83A8A6237"
+
+    def getResultJson(self, responseRaw, json_query):
         try:
             jsonString = responseRaw.json()['Result']
             jsonObj = json.loads(jsonString)
@@ -83,6 +176,5 @@ class OurCloudRequestHandler:
             print("JSON parse error: ", e.args[0])
             return None
         else:
-            requestStatus = jmespath.search("[?RequestNo == `{no}`].{attribute}|[0]".format(no=self.requestno,
-                                            attribute=attribute), jsonObj)
+            requestStatus = jmespath.search(json_query, jsonObj)
             return requestStatus
