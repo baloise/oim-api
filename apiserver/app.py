a1a5463cb8275c41baefaf324ba2d6702fc8e894
Grey, Georges <georges.grey@baloise.com>
2021-02-25 10:41:12 +0100

    add logging, oimlogger usage of the .env for parm

--- apiserver/app.py
+++ apiserver/app.py
@@ -3,13 +3,37 @@ from flask import render_template
 import os
 import re
 from dotenv import load_dotenv
+import logging
+import logging.config
+
 
 # Load non-yet-set envvars from .env file if it exists
 load_dotenv()
 
+# start logging, get parameters from config file and load oim logging spec
+logger_config_file = os.environ.get("LOGGING_CONFIGFILE")
+logger_log_file = os.environ.get("LOGGING_LOGFILE")
+logger_mailhost = os.environ.get("LOGGING_MAILHOST")
+logging.config.fileConfig(logger_config_file,
+                          defaults={'logfilename': logger_log_file,
+                                    'mailhost': logger_mailhost,
+                                    'toaddrs': 'foo@foo.ch'})
+logger = logging.getLogger('oimLogger')
+
+# start using our logger
+logger.info('OIM logger startet')
+# test different levels
+logger.debug('This is an debug message')
+logger.info('This is a info message')
+logger.warning('This is a warning message')
+logger.error('This is a error message')
+logger.critical('This is a critical message')
+
 # Now we create the webapp object
-server_port = os.getenv('SERVER_PORT') or 9090
-app = connexion.FlaskApp(__name__, port=server_port, specification_dir='openapi/')
+# server_port and spec_dir are now from .env file
+server_port = os.environ.get("SERVER_PORT")
+spec_dir = os.environ.get("SPECDIR")
+app = connexion.FlaskApp(__name__, port=server_port, specification_dir=spec_dir)
 
 # Next we tell it to load our API specs
 app.add_api('demoapi.yaml')
@@ -25,7 +49,7 @@ app.add_api('oimtest.yaml')
 # app.add_api('oimapi-v0.1.yaml')
 
 # API spec used for testing the order and cmdb classes
-app.add_api('oimtest_manu.yaml')
+# app.add_api('oimtest_manu.yaml')
 
 # Next we create an object called application that points to our webapp
 # This is only needed when the webapp is loaded by a production-ready application server
