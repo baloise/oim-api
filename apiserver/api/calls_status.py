4fb6d1ea0a9b11ac4cf93b2f03f75df8114af7c6
Vangual <vangual@users.noreply.github.com>
2021-03-19 15:56:57 +0100

    Fix circular dependency

--- apiserver/api/calls_status.py
+++ apiserver/api/calls_status.py
@@ -1,8 +1,8 @@
-from datetime import datetime
 from app import db
-from models.orders import OrderType, Person, Order, OrderStateType, BackendType, OrderStatus
+from models.orders import OrderType, Person
 from workflows.Factory import OrderFactory
 from oim_logging import get_oim_logger
+import api.util_status
 
 
 def retrieve_user(id=None, username=None, email=None, create_if_missing=False):
@@ -59,49 +59,7 @@ def create_minimal_order(orderinfo):
 
 
 def create_status(status):
-    log = get_oim_logger()
-    osStateValues = set(item.state for item in OrderStateType)
-    stateName = status.get('state', OrderStateType.NEW.state)
-    if stateName not in osStateValues:
-        log.warn('Illegal state given: {name}'.format(name=stateName))
-        return 'Illegal state given', 400
-
-    btStateValues = set(item.value for item in BackendType)
-    if status.get('system') not in btStateValues:
-        log.warn('Illegal system given: {}'.format(str(status.get('system'))))
-        return 'Illegal system given', 400
-
-    order_id = status.get('orderid', None)
-    if not order_id:
-        log.warn('create_status called without orderid!')
-        return 'Order not found', 404
-    order = db.session.query(Order).filter(Order.id == order_id).one_or_none()
-    if not order:
-        log.info('create_status() called with non-existent order id: {oid}'.format(
-            oid=str(order_id)
-        ))
-        return 'Order not found', 404
-    else:
-        log.debug('Successfully retreived order item for id: {oid}'.format(
-            oid=str(order_id)
-        ))
-    log.debug('Constructing new orderstatus item')
-
-    new_status = OrderStatus(
-        state=stateName,
-        since=status.get('since', datetime.now()),
-        system=status.get('system')
-    )
-
-    log.debug("Adding new status {stat} to order object {oid}".format(
-        stat=stateName,
-        oid=str(order_id)))
-    order.history.append(new_status)
-    db.session.commit()
-    log.debug('All done, new status created with id: {sid}'.format(
-        sid=new_status.id
-    ))
-    return {'statusid': new_status.id}, 201
+    return api.util_status.create_status(status)
 
 
 def list_statuses(orderid):
