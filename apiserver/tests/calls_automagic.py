63f3faebcf7db5a84860771fe3bfa6eff54f0e5f
Samuel Graenacher <vangual@users.noreply.github.com>
2021-12-10 17:16:05 +0100

    Fix tests

--- apiserver/tests/calls_automagic.py
+++ apiserver/tests/calls_automagic.py
@@ -4,25 +4,6 @@ from api.calls_automagic import validateYML
 
 
 class TestValidateYML(unittest.TestCase):
-    valid_yaml_vm = """
-apiVersion: v1alpha1
-kind: VirtualMachine
-metadata:
-  name: corellia
-  serviceOwner: b012345
-  labels:
-    environment: test
-spec:
-  status: active # one of active / decommissioned / deleted
-  id: 28f5a2fb-ecce-4dd6-9b89-bc2319da9634
-  adminRole: F-AAD-APP-XYZ-SH-T-BizDevOps-Engineer
-  ServerSizeCode: S1
-  DriveSize : 5
-  operatingSystem:
-    kind: RHEL
-    version: 8
-    """
-
     def obj_to_yaml(self, the_object):
         return yaml.dump(the_object)
 
@@ -38,7 +19,7 @@ spec:
 
     def test_200_nokind(self):
         # kind attrib missing
-        with open('schemas/yamlvalidation/references/postgres-db-yaml.yml') as f:
+        with open('schemas/yamlvalidation/references/postgres-db-yaml.yaml') as f:
             body = f.read()
             obj = self.yaml_to_obj(body)
         del obj['kind']
@@ -49,7 +30,7 @@ spec:
 
     def test_201_unknown_kind(self):
         # kind points to something we have no schemahints for
-        with open('schemas/yamlvalidation/references/postgres-db-yaml.yml') as f:
+        with open('schemas/yamlvalidation/references/postgres-db-yaml.yaml') as f:
             body = f.read()
             obj = self.yaml_to_obj(body)
         obj['kind'] = 'FakeKindUnitTests'
